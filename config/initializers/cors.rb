91236ed4298b475bc804f7d2e6b63bbc2327879b
TJ Gore <gorejrtyndale@gmail.com>
2019-01-06 13:51:30 +0800

    Update cors orgins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://localhost:3000', 'https://andio-react'
+     origins 'http://localhost:3000', 'https://andio-react.surge.sh'
 
      resource '*',
        headers: :any,
